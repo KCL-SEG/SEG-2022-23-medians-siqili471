caa8d7a687fd1d3502b6dda2e8109c2343ad9ea7
Siqi Li <1114673087@qq.com>
2023-09-30 20:09:52 +0100

    add a median function

--- median.py
+++ median.py
@@ -1,12 +1,18 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
+def median():
+    while True:
+        try:
+            print("Enter a list of numbers separated by commas: ")
+            numbers = [float(value) for value in input().split(",")]
+            if len(numbers) == 0:
+                return 0
+            elif len(numbers) % 2 == 0:
+                return (numbers[int(len(numbers)/2 -1)] + numbers[int(len(numbers)/2)]) / 2
+            elif len(numbers) % 2 != 0:
+                return numbers[int(len(numbers) // 2)]
+        except ValueError:
+            print("Some input could not be converted to a number!")
 
-while True:
-    try:
-        print("Enter a list of numbers separated by commas: ")
-        numbers = [float(value) for value in input().split(",")]
-    except ValueError:
-        print("Some input could not be converted to a number!")
-    else:
-        break
-print(numbers)
+        else:
+            break
